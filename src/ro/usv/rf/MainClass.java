f9d629e829232708cf5ee51629450eae886949f7
vost4o <vasyl.ostafi@gmail.com>
2020-03-08 20:38:22 +0200

    Implements all the necessary methods from the StatisticsUtils class

--- src/ro/usv/rf/MainClass.java
+++ src/ro/usv/rf/MainClass.java
@@ -6,99 +6,112 @@ import java.util.Map;
 
 public class MainClass {
 	
-	
 	public static void main(String[] args) {
-		double[][] learningSet = FileUtils.readLearningSetFromFile("e:\\in.txt");
-		
+		double[][] learningSet = FileUtils.readLearningSetFromFile("in.txt");
+
 		int numberOfForms = learningSet.length;
 		int numberOfFeatures = learningSet[0].length;
 		
-		//calculate average for each feature
-		for (int featureIndex = 0; featureIndex < numberOfFeatures; featureIndex++)
-		{
+		double[] featureAverages = new double[numberOfFeatures];
+
+		// calculate average for each feature
+		for (int featureIndex = 0; featureIndex < numberOfFeatures; featureIndex++) {
 			Double[] feature = new Double[numberOfForms];
-			for (int formIndex = 0; formIndex < numberOfForms; formIndex++)
-			{
+			for (int formIndex = 0; formIndex < numberOfForms; formIndex++) {
 				feature[formIndex] = learningSet[formIndex][featureIndex];
 			}
-			double featureAverage = StatisticsUtils.calculateFeatureAverage(feature);
-			System.out.println("Feature average is : " + featureAverage);
+			featureAverages[featureIndex] = StatisticsUtils.calculateFeatureAverage(feature);
+			System.out.println("Feature average is : " + featureAverages[featureIndex]);
 		}
-		
-		//get weights from matrix - only after you add the new line !!!
-		// since the last column represent the weights, the number of features is now learningSet[0].length - 1
+
+		// get weights from matrix - only after you add the new line !!!
+		// since the last column represent the weights, the number of features is now
+		// learningSet[0].length - 1
 		numberOfFeatures = learningSet[0].length - 1;
-		
 
 		Double[] weights = new Double[numberOfForms];
-		for (int formIndex = 0; formIndex < numberOfForms; formIndex++)
-		{
-			// weights are always located on the last  column 
+		for (int formIndex = 0; formIndex < numberOfForms; formIndex++) {
+			// weights are always located on the last column
 			int weightColumnIndex = learningSet[formIndex].length - 1;
 			weights[formIndex] = learningSet[formIndex][weightColumnIndex];
 		}
-		
 
 		// calculate Feature Weighted Average and feature dispersion
 		List<Double[]> featureList = new ArrayList<Double[]>();
-		double[] featureWeightedAverages = new double[numberOfFeatures]; 
-		double[] featureDispersions = new double[numberOfFeatures]; 
-		double[] averageSquareDeviations = new double[numberOfFeatures]; 
-		
-		for (int featureIndex = 0; featureIndex < numberOfFeatures; featureIndex++)
-		{
+		double[] featureWeightedAverages = new double[numberOfFeatures];
+		double[] featureDispersions = new double[numberOfFeatures];
+		double[] averageSquareDeviations = new double[numberOfFeatures];
+
+		for (int featureIndex = 0; featureIndex < numberOfFeatures; featureIndex++) {
 
 			Double[] feature = new Double[numberOfForms];
-			for (int formIndex = 0; formIndex < numberOfForms; formIndex++)
-			{
+			for (int formIndex = 0; formIndex < numberOfForms; formIndex++) {
 				feature[formIndex] = learningSet[formIndex][featureIndex];
 			}
 			featureList.add(feature);
-			
-			featureWeightedAverages[featureIndex]= StatisticsUtils.calculateFeatureWeightedAverage(feature,weights);
+
+			featureWeightedAverages[featureIndex] = StatisticsUtils.calculateFeatureWeightedAverage(feature, weights);
 			System.out.println("Feature weighted average is : " + featureWeightedAverages[featureIndex]);
-			
-			featureDispersions[featureIndex] = StatisticsUtils.calculateFeatureDispersion(feature, featureWeightedAverages[featureIndex]);
+
+			featureDispersions[featureIndex] = StatisticsUtils.calculateFeatureDispersion(feature,
+					featureWeightedAverages[featureIndex]);
 			System.out.println("Feature dispersion is : " + featureDispersions[featureIndex]);
-			
-			averageSquareDeviations[featureIndex] = StatisticsUtils.calculateAverageSquareDeviation(featureDispersions[featureIndex]);
+
+			averageSquareDeviations[featureIndex] = StatisticsUtils
+					.calculateAverageSquareDeviation(featureDispersions[featureIndex]);
 			System.out.println("average Square Deviations is : " + averageSquareDeviations[featureIndex]);
 		}
-				
-		//we select a feature and an element to calculate frequency of occurence
-		
+
+		// we select a feature and an element to calculate frequency of occurence
+
 		int featureIndex = 0;
 		int elementIndex = 2;
-		Map<Double, Integer> featureDistincElementsCounterMap = 
-				StatisticsUtils.getFeatureDistincElementsCounterMap(featureList.get(featureIndex));
-		double frequencyOfOccurence = StatisticsUtils.calculateFrequencyOfOccurence(featureDistincElementsCounterMap, learningSet[elementIndex][featureIndex]);
+		Map<Double, Integer> featureDistincElementsCounterMap = StatisticsUtils
+				.getFeatureDistincElementsCounterMap(featureList.get(featureIndex));
+		double frequencyOfOccurence = StatisticsUtils.calculateFrequencyOfOccurence(featureDistincElementsCounterMap,
+				learningSet[elementIndex][featureIndex]);
 		System.out.println("frequency Of Occurence is : " + frequencyOfOccurence);
-		
-		
-		//we select two features to calculate covariance and corelation
+
+		// we select two features to calculate covariance and corelation
 		int feature1Index = 0;
 		int feature2Index = 1;
-		
-		double covariance = StatisticsUtils.calculateCovariance(featureList.get(feature1Index), featureList.get(feature2Index), 
-				featureWeightedAverages[feature1Index],  featureWeightedAverages[1]);
+
+		double covariance = StatisticsUtils.calculateCovariance(featureList.get(feature1Index),
+				featureList.get(feature2Index), featureWeightedAverages[feature1Index], featureWeightedAverages[1]);
 		System.out.println("covariance is : " + covariance);
-		
-		double corelation = StatisticsUtils.calculateCorrelationCoefficient(covariance, featureDispersions[feature1Index], featureDispersions[feature2Index]);
+
+		double corelation = StatisticsUtils.calculateCorrelationCoefficient(covariance,
+				featureDispersions[feature1Index], featureDispersions[feature2Index]);
 		System.out.println("corelation is : " + corelation);
-		
-		
-		FileUtils.writeLearningSetToFile("e:\\scaledSet.csv", autoscaleLearningSet(learningSet));
-		
-		
+
+		FileUtils.writeLearningSetToFile("scaledSet.csv",
+		autoscaleLearningSet(learningSet, featureAverages));
 
 	}
-	
-	private static double[][] autoscaleLearningSet (double[][] learningSet)
-	{
-		double[][] autoscaledLearningSet = new double[learningSet.length][];
-		//.. your code here
+
+	private static double[][] autoscaleLearningSet(double[][] learningSet, double[] featureAverages) {
+		
+		int numOfForms = learningSet.length;
+		int numOfFeatures = learningSet[0].length - 1;
+		
+		double[][] autoscaledLearningSet = new double[numOfForms][numOfFeatures];
+		
+		if(featureAverages != null && featureAverages.length > 0) {
+			for(int i = 0; i < numOfForms; ++i) {
+				for(int j = 0; j < numOfFeatures; ++j) {			
+					double firstDiff = learningSet[i][j] - featureAverages[i];
+					
+					double secondDiff = 0;
+					
+					for(int k = 0; k < numOfForms; ++k) {
+						secondDiff += Math.pow(learningSet[k][j] - featureAverages[i], 2);
+					}
+					
+					autoscaledLearningSet[i][j] = firstDiff / secondDiff;
+				}
+			}
+		}
+
 		return autoscaledLearningSet;
 	}
-
-	
 }
--- src/ro/usv/rf/StatisticsUtils.java
+++ src/ro/usv/rf/StatisticsUtils.java
@@ -3,9 +3,7 @@ package ro.usv.rf;
 import java.util.HashMap;
 import java.util.Map;
 
-public class StatisticsUtils 
-{
-
+public class StatisticsUtils {
 	protected static double calculateFeatureAverage(Double[] feature) {
 		Map<Double, Integer> counterMap = getFeatureDistincElementsCounterMap(feature);
 		double featureAverage = 0;
@@ -13,17 +11,13 @@ public class StatisticsUtils
 		double sum1 = 0;
 		double sum2 = 0;
 
-		sum1 = counterMap.keySet().stream()
-				.mapToDouble(x -> calculateSum1(x, counterMap.get(x))).sum();
-		sum2 = counterMap.values().stream()
-				.mapToInt(x -> x).sum();
+		sum1 = counterMap.keySet().stream().mapToDouble(x -> calculateSum1(x, counterMap.get(x))).sum();
+		sum2 = counterMap.values().stream().mapToInt(x -> x).sum();
 		featureAverage = sum1 / sum2;
-		System.out.println("The feature average is: " +  featureAverage);
 		return featureAverage;
-}
-	
-	protected static Map<Double, Integer> getFeatureDistincElementsCounterMap(Double feature[])
-	{
+	}
+
+	protected static Map<Double, Integer> getFeatureDistincElementsCounterMap(Double feature[]) {
 		Map<Double, Integer> counterMap = new HashMap<Double, Integer>();
 		for (int j = 0; j < feature.length; j++) {
 			if (counterMap.containsKey(feature[j])) {
@@ -35,47 +29,77 @@ public class StatisticsUtils
 		}
 		return counterMap;
 	}
-	
-	private static Double calculateSum1(double value, int count)
-	{
+
+	private static Double calculateSum1(double value, int count) {
 		return count * value;
+	}
+
+	private static Map<Double, Double> getWeightsMap(Double feature[], Double[] weights) {
+		Map<Double, Double> ponderMap = new HashMap<Double, Double>();
+		for (int j = 0; j < feature.length; j++) {
+			if (ponderMap.containsKey(feature[j])) {
+				double weightSum = ponderMap.get(feature[j]);
+				weightSum += weights[j];
+				ponderMap.put(feature[j], weightSum);
+			} else {
+				ponderMap.put(feature[j], weights[j]);
+			}
+		}
+		return ponderMap;
+	}
+
+	private static Double calculatePonderSum1(double value, double weight) {
+		return weight * value;
 	}
 
 	protected static double calculateFeatureWeightedAverage(Double[] feature, Double[] weights) {
 		double featureWeightedAverage = 0.0;
-		// your code here
+
+		Map<Double, Double> weightsMap = getWeightsMap(feature, weights);
+
+		double sum1 = weightsMap.keySet().stream().mapToDouble(x -> calculatePonderSum1(x, weightsMap.get(x))).sum();
+		double sum2 = weightsMap.values().stream().mapToDouble(x -> x).sum();
+
+		featureWeightedAverage = sum1 / sum2;
+
 		return featureWeightedAverage;
 	}
-	
+
 	protected static double calculateFrequencyOfOccurence(Map<Double, Integer> counterMap, double featureElement) {
-		double frequencyOfOccurence = 0.0;
-		// your code here
+		double frequencyOfOccurence = counterMap.get(featureElement)
+				/ counterMap.values().stream().mapToDouble(x -> x).sum();
 		return frequencyOfOccurence;
 	}
-	
+
 	protected static double calculateFeatureDispersion(Double[] feature, double featureWeightedAverage) {
 		double featureDispersion = 0.0;
-		// your code here
+		double sum = 0;
+		for (int k = 0; k < feature.length; ++k) {
+			sum += Math.pow(feature[k] - featureWeightedAverage, 2);
+		}
+		featureDispersion = (1.0 / (feature.length - 1)) * sum;
 		return featureDispersion;
 	}
-	
-	protected static double calculateCovariance (Double[] feature1, Double[] feature2,
-			double feature1WeightedAverage,double feature2WeightedAverage) {
+
+	protected static double calculateCovariance(Double[] feature1, Double[] feature2, double feature1WeightedAverage,
+			double feature2WeightedAverage) {
 		double covariance = 0.0;
-		// your code here
+		double sum = 0;
+		for (int k = 0; k < feature1.length; ++k) {
+			sum += (feature1[k] - feature1WeightedAverage) * (feature2[k] - feature2WeightedAverage);
+		}
+		covariance = (Double.valueOf(1) / (feature1.length - 1)) * sum;
 		return covariance;
 	}
-	
-	protected static double calculateCorrelationCoefficient  (double covariance, double feature1Dispersion, 
-			double feature2Dispersion ) {
-		double correlationCoefficient = 0.0;
-		// your code here
+
+	protected static double calculateCorrelationCoefficient(double covariance, double feature1Dispersion,
+			double feature2Dispersion) {
+		double correlationCoefficient = covariance / Math.sqrt(feature2Dispersion * feature1Dispersion);
 		return correlationCoefficient;
 	}
-	
-	protected static double calculateAverageSquareDeviation (double featureDispersion ) {
-		double averageSquareDeviation = 0.0;
-		// your code here
+
+	protected static double calculateAverageSquareDeviation(double featureDispersion) {
+		double averageSquareDeviation = Math.sqrt(featureDispersion);
 		return averageSquareDeviation;
 	}
 }
